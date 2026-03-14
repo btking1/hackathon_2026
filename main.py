0054be3c9e9bf5b3ef9f0485a90fe483c9558b17
Khuldi123 <ShaiWebsiteDeveloper@gmail.com>
2026-03-14 01:18:36 -0400

    Add files via upload
    Hackathon Test data and samples

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# import action_engine
-# import logger
-# import output_builder
-# import rule_engine
-import scanner
+# this was the old main file before we refactored
+# keeping it around "just in case"
 
-records = scanner.scan_folder()
+import requests
 
-if records is not None:
-    for record in records:
-        print(record)
+def get_weather(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=hardcoded_key_123"
+    r = requests.get(url)
+    return r.json()
+
+if __name__ == "__main__":
+    print(get_weather("London"))
---
+++ test_utils.py
@@ -0,0 +1,12 @@
+Resume - Dave Johnson
+=====================
+
+Objective: Seeking a senior developer position...
+
+Experience:
+- Software Engineer at TechCorp (2020-2024)
+- Junior Developer at StartupXYZ (2018-2020)
+
+Skills: Python, JavaScript, SQL, Cloud Services
+
+This is definitely not supposed to be in the project repo.
