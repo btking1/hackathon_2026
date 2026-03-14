0e2f56626fef08f40cd643776120f80023023e04
btking1 <btking1@my.waketech.edu>
2026-03-14 01:33:16 -0400

    organize dataset

--- datasets/main.py
+++ datasets/main.py
@@ -3,10 +3,12 @@
 
 import requests
 
+
 def get_weather(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=hardcoded_key_123"
     r = requests.get(url)
     return r.json()
+
 
 if __name__ == "__main__":
     print(get_weather("London"))
---
+++ index.py
@@ -0,0 +1,5 @@
+import action_engine
+import logger
+import output_builder
+import rule_engine
+import scanner
