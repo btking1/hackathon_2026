f370aa0ff4927e3222c202700b4100bd30dba1aa
btking1 <btking1@my.waketech.edu>
2026-03-13 23:34:53 -0400

    update logger

--- logger.py
+++ logger.py
@@ -8,14 +8,15 @@ from pathlib import Path
 # timestamp
 # success/failure reason
 
-original_dir = Path("/original_dir").mkdir(exist_ok=True)
-new_dir = Path("/new_folder").mkdir(exist_ok=True)
+original_dir = Path("original_dir").mkdir(exist_ok=True)
+new_dir = Path("new_folder").mkdir(exist_ok=True)
+
 
 rules = []
 actions = []
 is_file_success = True
 
-dir_info = Path("/new_folder").stat()
+dir_info = Path("new_folder").stat()
 
 
 new_folder_timestamp = datetime.datetime.fromtimestamp(dir_info.st_ctime)
