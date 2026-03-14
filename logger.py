33e6aad2c13fba9ead09a1e7320559e7af0eb45d
btking1 <btking1@my.waketech.edu>
2026-03-13 21:27:12 -0400

    add logger

--- logger.py
+++ logger.py
@@ -13,3 +13,18 @@ new_dir = Path("/new_folder").mkdir(exist_ok=True)
 
 rules = []
 actions = []
+is_file_success = True
+
+dir_info = Path("/new_folder").stat()
+
+
+new_folder_timestamp = datetime.datetime.fromtimestamp(dir_info.st_ctime)
+
+print(new_folder_timestamp)
+
+if is_file_success:
+    print("success")
+    # TODO
+else:
+    print("failed")
+    # TODO
